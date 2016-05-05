21cf8ebee87f803f72d80e07349a37ac1ef08d0f
Matthew Gamble <git@matthewgamble.net>
2016-05-05 19:27:33 +1000

    Refactor DiConfig class

--- src/DiConfig.php
+++ src/DiConfig.php
@@ -15,38 +15,90 @@ class DiConfig
      */
     public function createContainer() : Container
     {
-        $rootNs = "MattyG\\BBStatic\\";
         $di = (new ContainerBuilder())->newInstance(true);
+        $rootNs = "MattyG\\BBStatic\\";
 
         // Necessary evil, we need to be able to resolve objects based on runtime configuration
         $di->types["Aura\\Di\\Container"] = $di;
 
+        $this->initConfigConfig($di, $rootNs);
+        $this->initDirectoryManagerConfig($di, $rootNs);
+        $this->initBBCodeConfig($di, $rootNs);
+        $this->initSigningConfig($di, $rootNs);
+        $this->initPageConfig($di, $rootNs);
+        $this->initTemplateEngineConfig($di, $rootNs);
+        $this->initSymfonyFilesystemConfig($di, $rootNs);
+
+        return $di;
+    }
+
+    /**
+     * @param Container $di
+     * @param string $rootNs
+     */
+    private function initConfigConfig(Container $di, string $rootNs)
+    {
         $di->types[$rootNs . "Util\\ConfigFactory"] = $di->lazyGet("config_factory");
         $di->setters[$rootNs . "Util\\NeedsConfigTrait"]["setConfig"] = $di->lazyGet("config");
         $di->setters[$rootNs . "Util\\NeedsConfigFactoryTrait"]["setConfigFactory"] = $di->lazyGet("config_factory");
         $di->set("config", $di->lazyNew($rootNs . "Util\\Config", array("filename" => BBStatic::CONFIG_FILENAME)));
         $di->set("config_factory", $di->lazyNew($rootNs . "Util\\ConfigFactory"));
+    }
 
+    /**
+     * @param Container $di
+     * @param string $rootNs
+     */
+    private function initDirectoryManagerConfig(Container $di, string $rootNs)
+    {
         $di->types[$rootNs . "DirectoryManager"] = $di->lazyGet("directory_manager");
         $di->params[$rootNs . "DirectoryManager"]["config"] = $di->lazyGet("config");
         $di->setters[$rootNs . "NeedsDirectoryManagerTrait"]["setDirectoryManager"] = $di->lazyGet("directory_manager");
         $di->set("directory_manager", $di->lazyNew($rootNs . "DirectoryManager"));
+    }
 
+    /**
+     * @param Container $di
+     * @param string $rootNs
+     */
+    private function initBBCodeConfig(Container $di, string $rootNs)
+    {
         $di->types["Nbbc\\BBCode"] = $di->lazy(array($di->lazyNew($rootNs . "BBCode\\Init"), "init"));
         $di->setters[$rootNs . "NeedsFileBuilderTrait"]["setFileBuilder"] = $di->lazyGet("file_builder");
         $di->set("file_builder", $di->lazyNew($rootNs . "FileBuilder"));
+    }
 
+    /**
+     * @param Container $di
+     * @param string $rootNs
+     */
+    private function initSigningConfig(Container $di, string $rootNs)
+    {
         $di->types[$rootNs . "Signing\\Adapter\\SigningAdapterInterface"] = $di->lazyNew($rootNs . "Signing\\Adapter\\SigningAdapterInterfaceProxy");
         $di->params[$rootNs . "Signing\\SigningManager"]["config"] = $di->lazyGet("config");
         $di->params[$rootNs . "Signing\\Adapter\\GnuPG"]["options"] = $di->lazyGetCall("config", "getValue", "signing/gnupg", array());
         $di->setters[$rootNs . "Signing\\NeedsSignerTrait"]["setSigner"] = $di->lazyGet("signer");
         $di->set("signer", $di->lazyNew($rootNs . "Signing\\SigningManager"));
+    }
 
+    /**
+     * @param Container $di
+     * @param string $rootNs
+     */
+    private function initPageConfig(Container $di, string $rootNs)
+    {
         $di->setters[$rootNs . "Page\\NeedsPageFactoryTrait"]["setPageFactory"] = $di->lazyGet("page_factory");
         $di->setters[$rootNs . "Page\\NeedsPageRendererTrait"]["setPageRenderer"] = $di->lazyGet("page_renderer");
         $di->set("page_factory", $di->lazyNew($rootNs . "Page\\PageFactory"));
         $di->set("page_renderer", $di->lazyNew($rootNs . "Page\\Renderer"));
+    }
 
+    /**
+     * @param Container $di
+     * @param string $rootNs
+     */
+    private function initTemplateEngineConfig(Container $di, string $rootNs)
+    {
         $di->params["Mustache_Cache_FilesystemCache"]["baseDir"] = $di->lazyGetCall("directory_manager", "getCacheDirectory", "templates");
         $di->setters["Mustache_Engine"]["setCache"] = $di->lazyGet("template_cache");
         $di->setters["Mustache_Engine"]["setLoader"] = $di->lazyGet("template_loader");
@@ -60,11 +112,16 @@ class DiConfig
             "baseDir" => $di->lazyGetCall("directory_manager", "getTemplatePartialsDirectory"),
         )));
         $di->set("template_engine", $di->lazyNew("Mustache_Engine"));
+    }
 
+    /**
+     * @param Container $di
+     * @param string $rootNs
+     */
+    private function initSymfonyFilesystemConfig(Container $di, string $rootNs)
+    {
         $di->types["Symfony\\Component\\Filesystem\\Filesystem"] = $di->lazyGet("filesystem");
         $di->setters[$rootNs . "Util\\Vendor\\NeedsFilesystemTrait"]["setFilesystem"] = $di->lazyGet("filesystem");
         $di->set("filesystem", $di->lazyNew("Symfony\\Component\\Filesystem\\Filesystem"));
-
-        return $di;
     }
 }
