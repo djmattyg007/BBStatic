40c55969da7f0f3e84b8a6f9f3de7724d99c9806
Matthew Gamble <git@matthewgamble.net>
2016-06-18 15:44:22 +1000

    Fix url_map BBCode tag
    It wasn't putting whitespace around the resulting anchor tag properly.

--- src/BBCode/rules/url_map.php
+++ src/BBCode/rules/url_map.php
@@ -7,11 +7,8 @@ return array(
     "mode" => \Nbbc\BBCode::BBCODE_MODE_CALLBACK,
     "method" => new \MattyG\BBStatic\Util\Vendor\LazyCallable($di->lazyNew("MattyG\\BBStatic\\BBCode\\Rule\\URLMap")),
     "class" => "link",
+    "content" => \Nbbc\BBCode::BBCODE_REQUIRED,
     "allow_in" => array("listitem", "block", "columns", "inline"),
-    "before_tag" => "sns",
-    "after_tag" => "sns",
-    "before_endtag" => "sns",
-    "after_endtag" => "sns",
     "plain_start" => "<b>URL:</b>\n",
     "plain_end" => "\n",
 );
