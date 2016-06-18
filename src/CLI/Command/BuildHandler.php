2216ca7c104e29a0dbb6199fc7671ee3fc5fddd1
Matthew Gamble <git@matthewgamble.net>
2016-06-18 15:50:28 +1000

    Add posts to main build command

--- src/CLI/Command/BuildHandler.php
+++ src/CLI/Command/BuildHandler.php
@@ -5,6 +5,8 @@ namespace MattyG\BBStatic\CLI\Command;
 
 use MattyG\BBStatic\Content\Page\NeedsPageBuilderTrait;
 use MattyG\BBStatic\Content\Page\NeedsPageGathererTrait;
+use MattyG\BBStatic\Content\Post\NeedsPostBuilderTrait;
+use MattyG\BBStatic\Content\Post\NeedsPostGathererTrait;
 use MattyG\BBStatic\Util\NeedsConfigTrait;
 use Webmozart\Console\Api\Args\Args;
 use Webmozart\Console\Api\IO\IO;
@@ -14,6 +16,8 @@ class BuildHandler
     use NeedsConfigTrait;
     use NeedsPageBuilderTrait;
     use NeedsPageGathererTrait;
+    use NeedsPostBuilderTrait;
+    use NeedsPostGathererTrait;
     use ShouldSignOutputTrait;
 
     /**
@@ -24,10 +28,31 @@ class BuildHandler
     {
         $shouldSignOutput = $this->shouldSignOutput($args);
         $io->writeLine("Signing output: " . ($shouldSignOutput === true ? "true" : "false"));
+        $this->buildPages($io, $shouldSignOutput);
+        $this->buildPosts($io, $shouldSignOutput);
+    }
 
+    /**
+     * @param IO $io
+     * @param bool $shouldSignOutput
+     */
+    private function buildPages(IO $io, bool $shouldSignOutput)
+    {
         $pageCollection = $this->pageGatherer->gatherPages();
         $io->writeLine(sprintf("Found %d pages", count($pageCollection)));
 
         $this->pageBuilder->buildPages($pageCollection, $shouldSignOutput);
+    }
+
+    /**
+     * @param IO $io
+     * @param bool $shouldSignOutput
+     */
+    private function buildPosts(IO $io, bool $shouldSignOutput)
+    {
+        $postCollection = $this->postGatherer->gatherPosts();
+        $io->writeLine(sprintf("Found %d posts", count($postCollection)));
+
+        $this->postBuilder->buildPosts($postCollection, $shouldSignOutput);
     }
 }
